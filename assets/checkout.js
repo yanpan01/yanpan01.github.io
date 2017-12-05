ca331393628d78342b2d640c8125699691720bc0
Pan <yanpan@amazon.com>
2017-12-05 14:21:50 -0800

    Adding close for modal

---
+++ assets/checkout.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("adModal");
+
+var btn = document.getElementById("parent");
+
+var span = document.getElementByClassName("close")[0];
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
